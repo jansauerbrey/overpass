806d700d83bf38a1d9b1774009bbc7d98fed09ec
Jan Sauerbrey <jan@sby.de>
2016-04-06 15:30:29 +0200

    second try

--- R/rel_utils.r
+++ R/rel_utils.r
@@ -1,6 +1,6 @@
 # handles making of a Lines list from the way elements
 # NOTE: ways can be polygons; need to figure that out
-process_osm_rels <- function(doc, osm_nodes) {
+process_osm_rels <- function(doc) {
 
   # get all the centers
   tmp <- xml_attrs(xml_find_all(doc, "//center"))
@@ -8,7 +8,7 @@ process_osm_rels <- function(doc, osm_nodes) {
   relations <- relations[, c("lon", "lat")]
   
   # need numeric lon/lat
-  mutate(nodes, lon=as.numeric(lon), lat=as.numeric(lat))
+  mutate(relations, lon=as.numeric(lon), lat=as.numeric(lat))
 
 }
 #osm_nodes_to_sptsdf <- function(osm_nodes) {
