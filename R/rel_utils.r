de7d89b82a2f6ba53c29fb0ea2e2a411a4f5dc91
Jan Sauerbrey <jan@sby.de>
2016-04-08 01:38:31 +0200

    better handling hopefully

--- R/rel_utils.r
+++ R/rel_utils.r
@@ -1,19 +1,34 @@
-# handles making of a Lines list from the way elements
-# NOTE: ways can be polygons; need to figure that out
+# find all the nodes and store them in a data frame
 process_osm_relations <- function(doc) {
 
-  # get all the relations
+  # efficiently find all node attributes (lat/lon)
+  # and get them into a data frame
   tmp <- xml_attrs(xml_find_all(doc, "//relation"))
   relations <- as.data.frame(t(do.call(cbind, tmp)), stringsAsFactors=FALSE)
-  relations <- relations[,"id"]
+  relations <- relations[, c("id")]
   
   tmp <- xml_attrs(xml_find_all(doc, "//center"))
   centers <- as.data.frame(t(do.call(cbind, tmp)), stringsAsFactors=FALSE)
   relations <- cbind(relations, centers[, c("lon", "lat")])
-  
+
+  # find all the nodes with tags
+  relations_with_tags <- tryCatch(xml_find_all(doc, "//relation[child::tag]"),
+                              errror=function(err){ return(list(0)) })
+
+  # if there are any, add the tag key/value to make a wide data frame
+  if (length(relations_with_tags) > 0) {
+    bind_rows(lapply(relations_with_tags, function(x) {
+      v <- xml_attr(xml_find_all(x, "tag"), "v")
+      names(v) <- xml_attr(xml_find_all(x, "tag"), "k")
+      pts <- cbind.data.frame(id=xml_attr(x, "id"), t(v),
+                              stringsAsFactors=FALSE)
+    })) -> relation_attrs
+    relations <- left_join(relations, relation_attrs, by="id")
+  }
+
   # need numeric lon/lat
   mutate(relations, lon=as.numeric(lon), lat=as.numeric(lat))
-
+  
 }
 #osm_nodes_to_sptsdf <- function(osm_nodes) {
 #  df <- data.frame(filter(osm_nodes, -lon, -lat))
